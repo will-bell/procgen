9d9880b501f07b7f4eee4e0df688f17078553f6d
William Bell <wjbell97@gmail.com>
2020-11-13 15:06:03 -0500

    Added documentation and more type hinting for DomainConfig and BossfightDomainConfig

--- procgen/domains.py
+++ procgen/domains.py
@@ -1,5 +1,5 @@
 import json
-from typing import Union, Dict, Any
+from typing import Union, Dict, Any, Optional
 import warnings
 import time
 import os
@@ -11,10 +11,21 @@ Number = Union[int, float]
 
 
 def datetime_name():
-    return datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    return datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
 
-class Domain:
+class DomainConfig:
+    """
+    Base class for configuring game parameters in Procgen
+    """
+
+    update_time: int
+
+    _cache_name: str
+
+    cache_directory: Optional[str]
+
+    path: Optional[str]
 
     def __init__(self, cache_name: str = None):
         self.update_time = self._timestamp()
@@ -24,18 +35,45 @@ class Domain:
         self.cache_directory = None
         self.path = None
 
+    def __repr__(self) -> str:
+        statement = f'{self.__class__.__name__} {{\n'
+        for key, value in self.__dict__.items():
+            statement += f'   {key}: {value}\n'
+        statement += '}'
+        return statement
+
     @staticmethod
     def _timestamp() -> int:
+        """Get an integer timestamp. Useful for naming things
+
+        :return: timestamp
+        """
         return int(time.time() * 1000)
 
     def _json_data(self) -> Dict[str, Any]:
+        """Get the attributes that we want to serialize to json
+
+        All attributes that are not private will be written to the json file.
+
+        :return: filtered version of the instance's __dict__
+        """
         return {k: v for k, v in self.__dict__.items() if k[0] != '_'}
 
     def _to_json(self, path: str):
+        """Write the configuration to a json file
+
+        :param path: desired path for the json file
+        :return: None
+        """
         with open(str(path), 'w') as json_file:
             json.dump(self._json_data(), json_file)
 
     def to_json(self, path: str):
+        """Write the configuration to a json file and set the path for later use
+
+        :param path: desired path for the json file
+        :return: None
+        """
         if path != self.path:
             path_obj = pathlib.Path(path).absolute()
             self.path = str(path_obj)
@@ -48,7 +86,12 @@ class Domain:
             self._to_json(self.path)
 
     @classmethod
-    def from_json(cls, path: str):
+    def from_json(cls, path: str) -> 'DomainConfig':
+        """Load a configuration from a json file
+
+        :param path: path to the json file containing the configuration parameters
+        :return: configuration loaded from the file
+        """
         obj = cls()
         with open(path) as json_file:
             data = json.load(json_file)
@@ -59,14 +102,18 @@ class Domain:
 
         return obj
 
-    def __repr__(self) -> str:
-        statement = f'{self.__class__.__name__} {{\n'
-        for key, value in self.__dict__.items():
-            statement += f'   {key}: {value}\n'
-        statement += '}'
-        return statement
-
     def update_parameters(self, parameters: Dict[str, Number], cache: bool = True):
+        """Update a configuration in place and optionally cache its current state
+
+        If a configuration has already been written to a json file, then this method will update the json file with the
+        new parameters. The path to the json file will not change. If we wish to cache the current state of the
+        configuration before the parameters are changed, then the data will be copied to a new json file in the cache
+        directory.
+
+        :param parameters: new parameters for the configuration
+        :param cache: cache the current state of the configuration
+        :return: None
+        """
         if cache and self.path:
             # Cache the current state of the environment configuration before modifying it.
             # In the future, this should maintain some graph structure to see how the configuration branches throughout
@@ -88,8 +135,27 @@ class Domain:
                 json.dump(self.__dict__, json_file)
 
 
-class BossfightDomainConfig(Domain):
+class BossfightDomainConfig(DomainConfig):
+    """
+    Class for configuring the Bossfight game in Procgen
+    """
 
+    min_n_rounds: int
+    max_n_rounds: int
+
+    min_n_barriers: int
+    max_n_barriers: int
+
+    min_boss_round_health: int
+    max_boss_round_health: int
+    min_boss_invulnerable_duration: int
+    max_boss_invulnerable_duration: int
+    n_boss_attack_modes: int
+    min_boss_bullet_velocity: float
+    max_boss_bullet_velocity: float
+    boss_rand_fire_prob: float
+    boss_scale: float = 1.
+    
     def __init__(self,
                  min_n_rounds: int = 1,
                  max_n_rounds: int = 6,
@@ -126,9 +192,3 @@ class BossfightDomainConfig(Domain):
         self.max_boss_bullet_velocity = max_boss_bullet_velocity
         self.boss_rand_fire_prob = boss_rand_fire_prob
         self.boss_scale = boss_scale
-
-
-if __name__ == '__main__':
-    config = BossfightDomainConfig()
-    config.to_json('test_configs/test_config.json')
-    config.update_config({'min_n_rounds': 4})
